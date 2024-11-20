b283efb23d8ebfa902c523aee58555cdbf1c607d
4ndr-34 <93264963+4ndr-34@users.noreply.github.com>
2024-11-20 20:33:33 +0100

    some basic changes for multiple instances of one ws and H2 connection

--- users-service/src/main/java/com/users/userservice/controller/UsersController.java
+++ users-service/src/main/java/com/users/userservice/controller/UsersController.java
@@ -1,5 +1,8 @@
 package com.users.userservice.controller;
 
+import com.netflix.discovery.converters.Auto;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,9 +11,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/users")
 public class UsersController {
 
+    @Autowired
+    private Environment env;
+
     @GetMapping("/status")
     public String status() {
-        return "Status is OK";
+        return "Working on port " + env.getProperty("local.server.port");
     }
 
 
